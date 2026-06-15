6fa5ef446a5f0eb4601a0db249e485648d2bf35b
larissackk <larissa.alcantara.gomes@escola.pr.gov.br>
2026-06-15 08:49:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,29 @@
 const express = require ("express")
 const app = express ()
 const port = 3000
+app.use (express.json())
+const fs = require ('fs')
+
+app.post("/clientes", (req,res) => {
+  const cliente = req.body
+  // abrir o arquivo
+  try { 
+    const bd =  JSON.parse (fs.readFileSync ("bd.json", "utf8"))
+    
+    // adicionar o cliente
+    bd.push (cliente)
+    
+    //salvar o arquivo
+    fs.writeFileSync ("bd.json", JSON.stringify (bd), "utf8")
+   
+    //resposta
+    res.status(200).json({resposta: "Cliente cadastrado!"})
+ } 
+ catch (erro){ 
+    res.status(500). json ({erro: erro.message})
+  }
+  
+})
 
 app.get("/perfil", (req, res) => {
     res.json({nome : "Amandaaa" , idade : "15 anos"})
