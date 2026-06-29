a5a10f20ebede87713a64f93e1d8bfc40d428c32
larissackk <larissa.alcantara.gomes@escola.pr.gov.br>
2026-06-29 08:09:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // npm init
 // npm i express
+// instalar Rapidapi Client
 const express = require ("express")
 const app = express ()
 const port = 3000
@@ -8,7 +9,7 @@ const fs = require ('fs')
 
 app.post("/clientes", (req,res) => {
   const cliente = req.body
-  // abrir o arquivo
+    // abrir o arquivo
   try { 
     const bd =  JSON.parse (fs.readFileSync ("bd.json", "utf8"))
     
@@ -20,17 +21,67 @@ app.post("/clientes", (req,res) => {
    
     //resposta
     res.status(200).json({resposta: "Cliente cadastrado!"})
- } 
+     } 
  catch (erro){ 
     res.status(500). json ({erro: erro.message})
-  }
-  
-})
+   }
+    })
+    // abrir o arquivo/ banco de dados
+     app.get("/clientes", (req,res) => {
+        try {
+        const bd =  JSON.parse (fs.readFileSync ("bd.json", "utf8"))
+    res.status(200).json({resposta: bd})
+    } catch (erro) {
+         res.status (500).json({erro: erro.message})
+
+      }}
+
+      ) 
 
 app.get("/perfil", (req, res) => {
     res.json({nome : "Amandaaa" , idade : "15 anos"})
 })
 
+
+app.get("/clientes/:cpf", (req,res) => {
+   //pegar cpf
+    const cpf = req.params.cpf
+    try {
+        const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+        const cliente =  bd.find((cliente) => cliente.cpf == cpf)
+    if(!cliente){
+        return res.status(404).json({})
+    }
+        res.status (200).json({resposta: cliente})
+    } catch (erro){
+          res.status (500).json({erro: erro.message})
+}
+})
+
+app.delete("/cliente/ : cpf", (req,res) => {
+   //pegar o cpf da rota
+         const cpf = req.params.cpf
+   try {
+    // abrir o banco de dados
+         const bd = JSON.parse (fs.readFileSync ("bd.json", "utf8"))
+    //encontrar o índice  do cliente a ser removido
+         const indiceCliente = bdfindIndex ((cliente) => cliente.cpf == cpf ) 
+        
+   //remover o índice da lista
+        if(!indiceCliente == -1) {
+             return res.status (404).json ({erro: "O cliente não existe"})
+   }
+         bd.splice(indiceCliente,1)
+   //dar uma resposta para o cliente
+     res.status (200).json ({resposta: "Cliente excluído com sucesso"})
+   } catch (erro) {} 
+      res.status(500).json({erro: erro.message})
+})
+
+
 app.listen(port, () => {
     console.log ("API executando na porta" + port)
 })
+                            
+//  GET http://localhost:3000/clientes
+// TESTAR TODAS AS ROTAS: post, get geral e get cpf
